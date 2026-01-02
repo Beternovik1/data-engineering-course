9d120525a3e8f8f3125a4fecd4d70e7edc493229
Edgar <edgaralfarohernandez15@gmail.com>
2026-01-02 01:42:55 -0800

    feature: Add first part of the imorting data intermediate course

---
+++ 03_python_importing_data_intermediate/import_url.py
@@ -0,0 +1,22 @@
+# URLLIB
+#   - Provides interface for fetching data across the web
+#   - urlopen() - accepts URLs instead of file names
+
+# Import package
+from urllib.request import urlretrieve
+
+# Import pandas
+import pandas as pd
+
+# Assign url of file: url
+url = 'https://assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
+
+
+# Save file locally
+urlretrieve(url, 'winequality-red.csv')
+
+
+# Read file into a DataFrame and print its head
+df = pd.read_csv('winequality-red.csv', sep=';')
+print(df.head())
+
