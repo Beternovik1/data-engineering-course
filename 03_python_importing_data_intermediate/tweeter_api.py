e98940bf87a90b38bbb864ac8124e145e455c191
Edgar Alfaro Hernández <edgaralfarohernandez15@gmail.com>
2026-01-04 00:31:10 -0800

    Update tweeter_api.py

--- 03_python_importing_data_intermediate/tweeter_api.py
+++ 03_python_importing_data_intermediate/tweeter_api.py
@@ -10,12 +10,6 @@
 # This library has a nice balance of usability and capability
 import tweepy, json
 
-consumer_key = 'IQA0CUc6S6Y752l20n2bWLpGG' # api_key
-consumer_secret = 'udo1EbDwiMNmbgUGiMu0iiI9WErt4Kb71DmQA2Ydb8RFB67q07' # api_key_secret
-# bearer_token = 'AAAAAAAAAAAAAAAAAAAAAEP76gEAAAAAjJX85gzXEpawunLisireW2o3q2g%3D7eBvV6g0xMFQ6ZhRyepMaZNOgBwhHcxBZvzeGupL7VLyI65ga9'
-access_token = '871382550807142400-7uYsKFhSn9L70t6xoOWV4J3sNsZ9umZ' # access_token
-access_token_secret = 'RuBMyNujgVy2GXspJaILvDof73pQIjDJpTsVpv4PBmUR3' # access_token_secret
-
 # Create Streaming object
 stream = tweepy.Stream(consumer_key, consumer_secret,
                        access_token, access_token_secret)
